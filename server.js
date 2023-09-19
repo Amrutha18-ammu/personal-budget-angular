b5d190f236159cd2220ac1e0202b903c372aa184
Amrutha Yarlagadda <amrutha1827@gmail.com>
2023-09-19 15:31:03 -0400

    Added cors for server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
  const express = require('express');
+ const cors = require('cors');
  const app = express();
  const port = 3000;
+
+ app.use(cors()); 
  
- app.use('/', express.static('public'));
-
  const budget = require('./budget-data.json')
-
- app.get('/test', (req,res)=>{
-    res.send ('Hello World');
- });
 
  app.get('/budget', (req,res) =>{
     res.json(budget);
@@ -17,5 +14,5 @@
 
  
  app.listen(port, () =>{
-    console.log(`Serving at http://localhost:${port}`)
+    console.log(` API Served at http://localhost:${port}`)
  });
